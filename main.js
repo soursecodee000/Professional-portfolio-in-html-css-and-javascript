81cd8d634e785cae3228776b7b124c5cb57c0069
Muhammad Ahmad <119831866+soursecodee000@users.noreply.github.com>
2024-01-24 16:59:03 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["PYTHON EXPERT", "DATABASE DEVELOPER","AUTOMATION EXPERT","SCRIPT WRITTER","MATLAB EXPERT", "WEB DEVELOPER"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
---
+++ main.py
@@ -0,0 +1,7 @@
+a=3
+b=4
+def add_sub():
+    c=a+b
+    d=a-b
+    print(c,d)
+
